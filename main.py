eaaa8b3769a1a3e57c326d23d10c2f41b745e6f7
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-11-30 03:32:16 -0600

    finished project

--- main.py
+++ main.py
@@ -5,18 +5,6 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 import csv
 
-'''
-Red underlines? Install the required packages first:
-Open the Terminal in PyCharm (bottom left).
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -33,16 +21,7 @@ class CafeForm(FlaskForm):
     power_rating = SelectField('Power Outlet Rating', choices=[(1, '🔌'),(2, '🔌🔌'),(3, '🔌🔌🔌'),(4, '🔌🔌🔌🔌'),(5, '🔌🔌🔌🔌🔌')], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
 
-
-# all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -62,12 +41,7 @@ def add_cafe():
                            f"{form.wifi_rating.data},"
                            f"{form.power_rating.data}")
             return redirect(url_for('cafes'))
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
-    # if form.validate_on_submit():
-    #     with open('cafe-data.csv', encoding='utf-8',) as csv_file:
-    #         csv_data = csv.writer(csv_file, delimiter=',')
+
     return render_template('add.html', form=form)
 
 
